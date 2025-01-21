1a7ef42ee7bc6377cfbff4b62f0722d2c6b2e701
Nicolás Ganfornina García <nikoganfornina@gmail.com>
2025-01-21 20:58:10 +0100

    Finiquitado

--- src/main/java/org/example/Gestion/Main.java
+++ src/main/java/org/example/Gestion/Main.java
@@ -1,11 +1,15 @@
 package org.example.Gestion;
 
 
+import org.example.DAO.animalesImpl;
+import org.example.entities.Animales;
+
 public class Main {
     public static void main(String[] args) {
         // TRABAJO REALIZO POR NICOLAS GANFORNINA GARCIA
         // IMPLEMENTACION DE HIBERNATE EN UN CRUD DE GESTION DE ANIMALES Y USUARIOS
         // MAIN REDUCIDO PARA PRUEBAS DE FUNCIONAMIENTO
         GestionApp.InicioPrograma();
+
     }
 }
